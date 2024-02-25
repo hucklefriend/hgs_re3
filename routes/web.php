61be20e5e3c6099d8573a91c15eb68fc67e64e53
yuki <webmaster@horrorgame.net>
2024-02-26 07:01:35 +0900

    hgs_re3

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+use \App\Http\Controllers\Admin\AdminController;
+// 管理用
+Route::group(['prefix' => 'admin'/*, 'middleware' => ['auth', 'can:admin', 'management']*/], function () {
+    // 管理
+    Route::get('/', [AdminController::class, 'top'])->name('管理');
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
