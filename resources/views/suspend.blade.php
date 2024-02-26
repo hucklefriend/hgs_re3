b48b73389fe0a71aa804d408d741b5c11e8b175f
yuki <webmaster@horrorgame.net>
2024-02-27 06:32:30 +0900

    休止中ページを反映

---
+++ resources/views/suspend.blade.php
@@ -0,0 +1,14 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ホラーゲームネットワーク</title>
+</head>
+<body>
+<h1>ホラーゲームネットワーク 休止中</h1>
+<p>
+    忙しくなってきてサービスを運営できそうもないので休止しています。<br>
+    暇になってきたら再開予定です。<br>
+    あまり書いてないけど、<a href="https://dev.horrorgame.net">こちらのブログ</a>で状況はお知らせするかもしれません。
+</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Route::group(['prefix' => 'admin'/*, 'middleware' => ['auth', 'can:admin', 'mana
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('suspend');
 });
