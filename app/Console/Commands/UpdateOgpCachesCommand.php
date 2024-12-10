4be21cac43ccb903b1983bdd5921ada20830859b
huckle <webmaster@horrorgame.net>
2024-12-11 05:30:20 +0900

    OGPのキャッシュ更新バッチ #198

---
+++ app/Console/Commands/UpdateOgpCachesCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\OgpCache;
+use Illuminate\Console\Command;
+
+class UpdateOgpCachesCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-ogp-caches';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $ogps = OgpCache::orderBy('updated_at', 'desc')->take(10)->get();
+
+        foreach ($ogps as $ogp) {
+            $ogp->fetch()->saveOrDelete();
+        }
+    }
+}
