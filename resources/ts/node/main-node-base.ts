8c7fa3a4205e4bf18bf15f690edba0e4716e712e
huckle <webmaster@horrorgame.net>
2025-06-04 18:10:48 +0900

    出現アニメーション

--- resources/ts/node/main-node-base.ts
+++ resources/ts/node/main-node-base.ts
@@ -12,14 +12,18 @@ export abstract class MainNodeBase extends NodeBase
     protected canvas: HTMLCanvasElement;
     protected canvasCtx: CanvasRenderingContext2D;
     protected subLinkNodes: SubLinkNode[] = [];
+    protected nodeHead: HTMLElement;
     protected subNodeContainer: HTMLElement;
     protected gradientEndAlpha: number;
     protected animationStartTime: number;
+    protected subGradientStartAlpha: number;
     protected maxSubEndOpacity: number;
     protected minSubEndOpacity: number;
     protected appearAnimationFunc: (() => void) | null;
     protected appearStatus: number;
-    
+    protected curveAppearProgress: number;
+    protected subCurveAppearProgress: number[];
+
     /**
      * コンストラクタ
      */
@@ -29,13 +33,18 @@ export abstract class MainNodeBase extends NodeBase
 
         this.canvas = nodeElement.querySelector('.node-canvas') as HTMLCanvasElement;
         this.canvasCtx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-        this.gradientEndAlpha = 0.3;
+        this.nodeHead = nodeElement.querySelector('.node-head') as HTMLElement;
+        
+        this.gradientEndAlpha = 0;
         this.animationStartTime = 0;
+        this.subGradientStartAlpha = 0;
         this.maxSubEndOpacity = 0.5;
         this.minSubEndOpacity = 0.1;
         this.appearAnimationFunc = null;
         this.subNodeContainer = nodeElement.querySelector('.sub-node-container') as HTMLElement;
         this.appearStatus = MainNodeBase.APPEAR_STATUS_NONE;
+        this.curveAppearProgress = 0;
+        this.subCurveAppearProgress = [0, 0, 0, 0];
 
         const subLinkNodeElements = this.subNodeContainer?.querySelectorAll('.sub-link-node') || [];
         this.subLinkNodes = Array.from(subLinkNodeElements)
@@ -92,6 +101,9 @@ export abstract class MainNodeBase extends NodeBase
             this.appearStatus = MainNodeBase.APPEAR_STATUS_APPEARING;
             this.appearAnimationFunc = this.appearAnimation;
             this.nodeElement.style.visibility = 'visible';
+            this.curveAppearProgress = 0;
+            this.animationStartTime = (window as any).hgn.timestamp;
+            this.gradientEndAlpha = 0;
         }
     }
 
@@ -100,8 +112,64 @@ export abstract class MainNodeBase extends NodeBase
      */
     protected appearAnimation(): void
     {
-        this.animationStartTime = (window as any).hgn.timestamp;
-        this.appearAnimationFunc = this.appearAnimation;
+        this.curveAppearProgress = this.getAnimationProgress(500);
+        if (this.curveAppearProgress >= 1) {
+            this.curveAppearProgress = 1;
+            this.gradientEndAlpha = 0.3;
+            this.appearAnimationFunc = this.appearSubNodesAnimation;
+            this.nodeHead.classList.remove('disappear');
+            this.animationStartTime = (window as any).hgn.timestamp;
+        }
+        
+        this.isDraw = true;
+    }
+
+    /**
+     * サブノードの出現アニメーション
+     */
+    protected appearSubNodesAnimation(): void
+    {
+        const progress = this.getAnimationProgress(1000);
+        console.log(progress);
+        if (progress >= 1) {
+            this.subCurveAppearProgress = [1, 1, 1, 1];
+            this.appearStatus = MainNodeBase.APPEAR_STATUS_APPEARED;
+
+            this.appearAnimationFunc = null;
+        }
+
+        this.subCurveAppearProgress[0] = progress * 2;
+        if (this.subCurveAppearProgress[0] > 1) {
+            this.subCurveAppearProgress[0] = 1;
+
+            if (this.subLinkNodes.length > 0) {
+                this.subLinkNodes[0].element.classList.remove('disappear');
+            }
+        }
+        this.subCurveAppearProgress[1] = progress * 1.5;
+        if (this.subCurveAppearProgress[1] > 1) {
+            this.subCurveAppearProgress[1] = 1;
+            if (this.subLinkNodes.length > 1) {
+                this.subLinkNodes[1].element.classList.remove('disappear');
+            }
+        }
+        this.subCurveAppearProgress[2] = progress * 1.2;
+        if (this.subCurveAppearProgress[2] > 1) {
+            this.subCurveAppearProgress[2] = 1;
+            if (this.subLinkNodes.length > 2) {
+                this.subLinkNodes[2].element.classList.remove('disappear');
+            }
+        }
+
+        this.subCurveAppearProgress[3] = progress;
+        if (this.subCurveAppearProgress[3] >= 1) {
+            this.subCurveAppearProgress[3] = 1;
+            if (this.subLinkNodes.length > 3) {
+                this.subLinkNodes[3].element.classList.remove('disappear');
+            }
+        }
+        
+        this.isDraw = true;
     }
 
     /**
@@ -133,41 +201,38 @@ export abstract class MainNodeBase extends NodeBase
         // キャンバスをクリア
         this.canvasCtx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        // 発光効果のためのグラデーション
-        const gradient = this.canvasCtx.createLinearGradient(0, 0, 0, this.canvas.height);
-        gradient.addColorStop(0, 'rgb(144, 255, 144)');   // 薄い緑色
-        gradient.addColorStop(1, 'rgba(0, 0, 0, 0.2)');   // 薄い緑色
-
         // 発光効果の設定
         this.canvasCtx.shadowColor = 'rgba(144, 238, 144, 0.5)';
         this.canvasCtx.shadowBlur = 10;
         this.canvasCtx.shadowOffsetX = 0;
         this.canvasCtx.shadowOffsetY = 0;
-        this.canvasCtx.strokeStyle = gradient;
         this.canvasCtx.lineWidth = 2;
         this.canvasCtx.lineCap = 'butt';
 
         const connectionPoint = this.getConnectionPoint();
 
-        this.drawCurvedLine(
-            15,
-            0,
-            connectionPoint.x,
-            connectionPoint.y
-        );
-
-        const canvasRect = this.canvas.getBoundingClientRect();
-
-        this.subLinkNodes.forEach((subLinkNode, index) => {
-            if (index >= 4) return; // 4回を超えたら処理をスキップ
-            this.drawChildCurvedLine(
+        if (this.curveAppearProgress > 0) {
+            this.drawCurvedLine(
+                15,
+                0,
                 connectionPoint.x,
-                connectionPoint.y,
-                subLinkNode.getConnectionPoint().x - canvasRect.left,
-                subLinkNode.getConnectionPoint().y - canvasRect.top,
-                index
+                connectionPoint.y
             );
-        });
+        }
+
+        if (this.subCurveAppearProgress[0] > 0) {
+            const canvasRect = this.canvas.getBoundingClientRect();
+            this.subLinkNodes.forEach((subLinkNode, index) => {
+                if (index >= 4) return; // 4回を超えたら処理をスキップ
+                this.drawChildCurvedLine(
+                    connectionPoint.x,
+                    connectionPoint.y,
+                    subLinkNode.getConnectionPoint().x - canvasRect.left,
+                    subLinkNode.getConnectionPoint().y - canvasRect.top,
+                    index
+                );
+            });
+        }
         
         this.isDraw = false;
     }
@@ -183,9 +248,17 @@ export abstract class MainNodeBase extends NodeBase
     {
         const controlX = startX;
         const controlY = endY;
+        let currentEndX = endX;
+        let currentEndY = endY;
+
+        // 進行度に応じてグラデーションの終了点を調整
+        if (this.curveAppearProgress < 1) {
+            currentEndX = startX + (endX - startX) * this.curveAppearProgress;
+            currentEndY = startY + (endY - startY) * this.curveAppearProgress;
+        }
 
         // カーブに沿ったグラデーションを作成
-        const gradient = this.canvasCtx.createLinearGradient(startX, startY, endX, endY);
+        const gradient = this.canvasCtx.createLinearGradient(startX, startY, currentEndX, currentEndY);
         gradient.addColorStop(0, 'rgba(144, 255, 144, 1)');    // 開始点（明るい緑）
         gradient.addColorStop(1, `rgba(144, 255, 144, ${this.gradientEndAlpha})`);   // 終了点
 
@@ -211,9 +284,20 @@ export abstract class MainNodeBase extends NodeBase
     {
         // ループ回数に応じて透明度を調整（maxSubEndOpacityからminSubEndOpacityまで徐々に減少）
         const opacity = this.maxSubEndOpacity - (loopCount * 0.1);
-        const endOpacity = Math.max(this.minSubEndOpacity, opacity - this.minSubEndOpacity);
+        let endOpacity = Math.max(this.minSubEndOpacity, opacity - this.minSubEndOpacity);
 
-        const gradient = this.canvasCtx.createLinearGradient(startX, startY, endX, endY);
+        let currentEndX = endX;
+        let currentEndY = endY;
+
+        // 進行度に応じてグラデーションの終了点を調整
+        if (this.subCurveAppearProgress[loopCount] < 1) {
+            currentEndX = startX + (endX - startX) * this.subCurveAppearProgress[loopCount];
+            currentEndY = startY + (endY - startY) * this.subCurveAppearProgress[loopCount];
+
+            endOpacity = endOpacity * this.subCurveAppearProgress[loopCount];
+        }
+
+        const gradient = this.canvasCtx.createLinearGradient(startX, startY, currentEndX, currentEndY);
         gradient.addColorStop(0, `rgba(100, 200, 100, ${this.gradientEndAlpha})`);   // 開始点の透明度
         gradient.addColorStop(1, `rgba(20, 80, 20, ${endOpacity})`);   // 終了点の透明度
 
