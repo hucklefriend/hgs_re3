5da8567ede2b027a1b5385843fe5534f029659de
huckle <webmaster@horrorgame.net>
2025-09-27 18:02:14 +0900

    mixinにしてもらった

--- resources/ts/node/link-tree-node.ts
+++ resources/ts/node/link-tree-node.ts
@@ -1,30 +1,10 @@
-import { HorrorGameNetwork } from "../horror-game-network";
-import { BasicNode } from "./basic-node";
-import { AppearStatus } from "../enum/appear-status";
-import { FreePoint } from "./parts/free-point";
 import { TreeNodeInterface } from "./interface/tree-node-interface";
-import { CurrentNode } from "./current-node";
-import { Util } from "../common/util";
-import { Point } from "../common/point";
-import { CurveCanvas } from "./parts/curve-canvas";
 import { ClickableNodeInterface } from "./interface/clickable-node-interface";
-import { NodeHeadClickable } from "./parts/node-head-clickable";
+import { LinkNodeMixin } from "./mixins/link-node-mixin";
 import { TreeNode } from "./tree-node";
 
 export class LinkTreeNode extends TreeNode implements ClickableNodeInterface
 {
-    private _isHomewardDisappear: boolean;
-
-    public get anchor(): HTMLAnchorElement
-    {
-        return this.nodeHead.titleElement as HTMLAnchorElement;
-    }
-
-    public get nodeHead(): NodeHeadClickable
-    {
-        return this._nodeHead as NodeHeadClickable;
-    }
-
     /**
      * コンストラクタ
      * @param nodeElement ノードの要素
@@ -32,10 +12,7 @@ export class LinkTreeNode extends TreeNode implements ClickableNodeInterface
     public constructor(nodeElement: HTMLElement, parentNode: TreeNodeInterface)
     {
         super(nodeElement, parentNode);
-
-        this._isHomewardDisappear = false;
     }
-
 
     /**
      * アニメーションの更新処理
@@ -45,109 +22,56 @@ export class LinkTreeNode extends TreeNode implements ClickableNodeInterface
         super.update();
     }
 
-    /**
-     * ホバー時の処理
-     */
+    // LinkNodeMixinのメソッドを委譲
+    public get anchor(): HTMLAnchorElement
+    {
+        return this.linkMixin.anchor;
+    }
+
+    public get nodeHead(): any
+    {
+        return this.linkMixin.nodeHead;
+    }
+
     public hover(): void
     {
-        this._nodeContentBehind?.hover();
-        this._animationStartTime = (window as any).hgn.timestamp;
-        this._updateGradientEndAlphaFunc = this.updateGradientEndAlphaOnHover;
+        this.linkMixin.hover();
     }
 
-    /**
-     * ホバー解除時の処理
-     */
     public unhover(): void
     {
-        this._nodeContentBehind?.unhover();
-
-        this._animationStartTime = (window as any).hgn.timestamp;
-        this._updateGradientEndAlphaFunc = this.updateGradientEndAlphaOnUnhover;
+        this.linkMixin.unhover();
     }
 
-    /**
-     * クリック時の処理
-     * @param e クリックイベント
-     */
     public click(e: MouseEvent): void
     {
-        this._isHomewardDisappear = true;
-
-        const hgn = (window as any).hgn as HorrorGameNetwork;
-        const currentNode = hgn.currentNode as CurrentNode;
-        currentNode.moveNode(this.anchor.href, this, false);
-
-        this.parentNode.prepareDisappear(this);
+        this.linkMixin.click(e);
     }
-
 
     public disappear(): void
     {
-        if (!this._isHomewardDisappear) {
+        if (!this.linkMixin._isHomewardDisappear) {
             super.disappear();
         } else {
-            const hgn = (window as any).hgn as HorrorGameNetwork;
-            const freePt = this.freePt;
-            const connectionPoint = this._nodeHead.getConnectionPoint();
-            this._updateGradientEndAlphaFunc = null;
-    
-            freePt.setPos(12, 0).setElementPos();
-            freePt.show();
-            this._nodeHead.nodePoint.hidden();
-            
-            this._nodeContentBehind?.disappear();
-            this._nodeContentTree?.disappear();
-
-            this._animationStartTime = hgn.timestamp;
-            this._appearStatus = AppearStatus.DISAPPEARING;
-            this._curveCanvas.gradientEndAlpha = 0;
-            this._nodeHead.disappear();
-            this._isDraw = true;
-
-            this._appearAnimationFunc = this.selectedDisappearAnimation;
+            this.linkMixin.executeHomewardDisappear();
         }
     }
 
-    /**
-     * 消滾アニメーション
-     */
-    protected selectedDisappearAnimation(): void
+    public selectedDisappearAnimation(): void
     {
-        const connectionPoint = this._nodeHead.getConnectionPoint();
-
-        const hgn = (window as any).hgn as HorrorGameNetwork;
-        const freePt = this.freePt;
-
-        this._curveCanvas.appearProgress = 1 - Util.getAnimationProgress(this._animationStartTime, 200);
-        if (this._curveCanvas.appearProgress === 0) {
-            this._curveCanvas.gradientEndAlpha = 0;
-            this._appearAnimationFunc = this.selectedDisappearAnimation2;
-
-            this._animationStartTime = hgn.timestamp;
-        } else {
-            // TODO: 12を計算で出す
-            this._curveCanvas.drawCurvedLine(new Point(12, 0), connectionPoint);
-
-            const pos = Util.getQuadraticBezierPoint(
-                0, 0,
-                connectionPoint.x, connectionPoint.y,
-                this._curveCanvas.appearProgress
-            );
-    
-            freePt.moveOffset(pos.x, pos.y);
-        }
-        
-        this._isDraw = true;
+        this.linkMixin.selectedDisappearAnimation();
     }
 
     public selectedDisappearAnimation2(): void
     {
-        this._isHomewardDisappear = false;
-        this._appearAnimationFunc = null;
-        this._appearStatus = AppearStatus.DISAPPEARED;
+        this.linkMixin.selectedDisappearAnimation2();
+    }
 
-        this.freePt.hide();
-        this._parentNode.homewardDisappear();
+    private get linkMixin(): LinkNodeMixin
+    {
+        if (!(this as any)._linkMixin) {
+            (this as any)._linkMixin = new LinkNodeMixin(this);
+        }
+        return (this as any)._linkMixin;
     }
 }
---
+++ resources/ts/node/mixins/link-node-mixin.ts
@@ -0,0 +1,147 @@
+import { HorrorGameNetwork } from "../../horror-game-network";
+import { AppearStatus } from "../../enum/appear-status";
+import { CurrentNode } from "../current-node";
+import { Util } from "../../common/util";
+import { Point } from "../../common/point";
+import { ClickableNodeInterface } from "../interface/clickable-node-interface";
+import { NodeHeadClickable } from "../parts/node-head-clickable";
+
+/**
+ * LinkNodeとLinkTreeNodeの共通機能を提供するmixin
+ */
+export class LinkNodeMixin
+{
+    public _isHomewardDisappear: boolean = false;
+    private _parentInstance: any;
+
+    constructor(parentInstance: any)
+    {
+        this._parentInstance = parentInstance;
+    }
+
+    public get anchor(): HTMLAnchorElement
+    {
+        return this._parentInstance._nodeHead.titleElement as HTMLAnchorElement;
+    }
+
+    public get nodeHead(): NodeHeadClickable
+    {
+        return this._parentInstance._nodeHead as NodeHeadClickable;
+    }
+
+    /**
+     * ホバー時の処理
+     */
+    public hover(): void
+    {
+        this._parentInstance._nodeContentBehind?.hover();
+        this._parentInstance._animationStartTime = (window as any).hgn.timestamp;
+        this._parentInstance._updateGradientEndAlphaFunc = this._parentInstance.updateGradientEndAlphaOnHover;
+    }
+
+    /**
+     * ホバー解除時の処理
+     */
+    public unhover(): void
+    {
+        this._parentInstance._nodeContentBehind?.unhover();
+
+        this._parentInstance._animationStartTime = (window as any).hgn.timestamp;
+        this._parentInstance._updateGradientEndAlphaFunc = this._parentInstance.updateGradientEndAlphaOnUnhover;
+    }
+
+    /**
+     * クリック時の処理
+     * @param e クリックイベント
+     */
+    public click(e: MouseEvent): void
+    {
+        this._isHomewardDisappear = true;
+
+        const hgn = (window as any).hgn as HorrorGameNetwork;
+        const currentNode = hgn.currentNode as CurrentNode;
+        currentNode.moveNode(this.anchor.href, this._parentInstance, false);
+
+        this._parentInstance.parentNode.prepareDisappear(this._parentInstance);
+    }
+
+    /**
+     * 消滾アニメーション
+     */
+    public selectedDisappearAnimation(): void
+    {
+        const connectionPoint = this.nodeHead.getConnectionPoint();
+
+        const hgn = (window as any).hgn as HorrorGameNetwork;
+        const freePt = this._parentInstance.freePt;
+
+        this._parentInstance._curveCanvas.appearProgress = 1 - Util.getAnimationProgress(this._parentInstance._animationStartTime, 200);
+        if (this._parentInstance._curveCanvas.appearProgress === 0) {
+            this._parentInstance._curveCanvas.gradientEndAlpha = 0;
+            this._parentInstance._appearAnimationFunc = this._parentInstance.selectedDisappearAnimation2;
+
+            this._parentInstance._animationStartTime = hgn.timestamp;
+        } else {
+            // TODO: 12を計算で出す
+            this._parentInstance._curveCanvas.drawCurvedLine(new Point(12, 0), connectionPoint);
+
+            const pos = Util.getQuadraticBezierPoint(
+                0, 0,
+                connectionPoint.x, connectionPoint.y,
+                this._parentInstance._curveCanvas.appearProgress
+            );
+    
+            freePt.moveOffset(pos.x, pos.y);
+        }
+        
+        this._parentInstance._isDraw = true;
+    }
+
+    /**
+     * 消滾アニメーション2
+     */
+    public selectedDisappearAnimation2(): void
+    {
+        this._isHomewardDisappear = false;
+        this._parentInstance._appearAnimationFunc = null;
+        this._parentInstance._appearStatus = AppearStatus.DISAPPEARED;
+
+        this._parentInstance.freePt.hide();
+        this._parentInstance._parentNode.homewardDisappear();
+    }
+
+    /**
+     * ホームワード消滾処理
+     */
+    protected handleHomewardDisappear(): void
+    {
+        const hgn = (window as any).hgn as HorrorGameNetwork;
+        const freePt = this._parentInstance.freePt;
+        const connectionPoint = this.nodeHead.getConnectionPoint();
+        this._parentInstance._updateGradientEndAlphaFunc = null;
+
+        freePt.setPos(12, 0).setElementPos();
+        freePt.show();
+        this.nodeHead.nodePoint.hidden();
+        
+        this._parentInstance._nodeContentBehind?.disappear();
+        // TreeNodeの場合は_nodeContentTreeも消滾させる
+        this._parentInstance._nodeContentTree?.disappear();
+
+        this._parentInstance._animationStartTime = hgn.timestamp;
+        this._parentInstance._appearStatus = AppearStatus.DISAPPEARING;
+        this._parentInstance._curveCanvas.gradientEndAlpha = 0;
+        this.nodeHead.disappear();
+        this._parentInstance._isDraw = true;
+
+        this._parentInstance._appearAnimationFunc = this._parentInstance.selectedDisappearAnimation;
+    }
+
+    /**
+     * ホームワード消滾処理を実行する
+     */
+    public executeHomewardDisappear(): void
+    {
+        this.handleHomewardDisappear();
+    }
+}
