95f31a0aa93d5a1ca8d24433a9bb09cdb0ed0d67
huckle <webmaster@horrorgame.net>
2026-02-20 18:26:25 +0900

    お知らせのツリー化

--- resources/views/infomation_detail.blade.php
+++ resources/views/infomation_detail.blade.php
@@ -4,17 +4,34 @@
 @section('current-node-title', $info->head)
 
 @section('current-node-content')
+    @if (!empty($info->header_text))
     <p>
-        {!! nl2br($info->body) !!}
+        {!! nl2br(e($info->header_text)) !!}
     </p>
+    @endif
     <div style="margin-top: 30px;font-size: 13px;">
-        掲載日時：{{ $info->open_at->format('Y-m-d H:i') }}<br>
-        @if($info->close_at->format('Y-m-d H:i') !== '2099-12-31 23:59')掲載終了日時：{{ $info->close_at->format('Y-m-d H:i') }}@endif
+        公開日時：{{ $info->open_at->format('Y-m-d H:i') }}<br>
+        @if($info->close_at->format('Y-m-d H:i') !== '2099-12-31 23:59')公開終了日時：{{ $info->close_at->format('Y-m-d H:i') }}@endif
     </div>
 @endsection
 
 
 @section('nodes')
+
+    @for ($i = 1; $i <= 10; $i++)
+        @if (!empty($info->{'sub_title_' . $i}) || !empty($info->{'sub_text_' . $i}))
+        <section class="node">
+            <div class="node-head">
+                <h2 class="node-head-text">{{ $info->{'sub_title_' . $i} }}</h2>
+                <span class="node-pt">●</span>
+            </div>
+            <div class="node-content basic">
+                {!! nl2br($info->{'sub_text_' . $i}) !!}
+            </div>
+        </section>
+        @endif
+    @endfor
+
     <section class="node tree-node">
         <div class="node-head">
             <h2 class="node-head-text">近道</h2>
@@ -35,7 +52,6 @@
                     </section>
                 </div>
             </section>
-
         
             @if (\Illuminate\Support\Facades\Auth::guard('admin')->check())
             <section class="node link-node">
