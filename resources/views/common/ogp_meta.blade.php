4d2adf39dcb86a210f485952aa8b6a88ceb66799
huckle <webmaster@horrorgame.net>
2026-02-23 04:27:33 +0900

    OGPの設定

---
+++ resources/views/common/ogp_meta.blade.php
@@ -0,0 +1,17 @@
+@php
+    $siteName = $siteName ?? 'ホラーゲームネットワーク(α)';
+    $ogpTitle = $ogpTitle ?? $siteName;
+    $ogpDescription = $ogpDescription ?? '';
+    $ogpImage = $ogpImage ?? asset('images/ogp.png');
+    $ogpUrl = $ogpUrl ?? url()->current();
+    $ogpType = $ogpType ?? 'website';
+@endphp
+<meta property="og:title" content="{{ $ogpTitle }}">
+<meta property="og:description" content="{{ $ogpDescription }}">
+<meta property="og:url" content="{{ $ogpUrl }}">
+<meta property="og:type" content="{{ $ogpType }}">
+<meta property="og:site_name" content="{{ $siteName }}">
+@if(!empty($ogpImage))
+<meta property="og:image" content="{{ $ogpImage }}">
+@endif
+<meta name="twitter:card" content="summary_large_image">
